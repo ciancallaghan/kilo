63fc1c535e97a69c74652f68a2be2c9ebaf5aaf4
Cian Callaghan <cian.callaghan@student.htw-berlin.de>
2023-04-01 11:55:48 +0200

    Added searching

--- kilo.c
+++ kilo.c
@@ -416,16 +416,35 @@ void editorSave() {
 /*** find ***/
 
 void editorFindCallback(char *query, int key) {
+	static int last_match = -1;
+	static int direction = 1;
+
 	if (key == '\r' || key == '\x1b') {
+		last_match = -1;
+		direction = 1;
 		return;
+	} else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+		direction = 1;
+	} else if (key == ARROW_LEFT || key == ARROW_UP) {
+		direction = -1;
+	} else {
+		last_match = -1;
+		direction = 1;
 	}
 
+	if (last_match == -1) direction = 1;
+	int current = last_match;
 	int i;
 	for (i = 0; i < E.numrows; i++) {
-		erow *row = &E.row[i];
+		current += direction;
+		if (current == -1) current = E.numrows - 1;
+		else if (current == E.numrows) current = 0;
+
+		erow *row = &E.row[current];
 		char *match = strstr(row->render, query);
 		if (match) {
-			E.cy = i;
+			last_match = current;
+			E.cy = current;
 			E.cx = editorRowRxToCx(row, match - row->render);
 			E.rowoff = E.numrows;
 			break;
@@ -434,10 +453,20 @@ void editorFindCallback(char *query, int key) {
 }
 
 void editorFind() {
-	char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+	int saved_cx = E.cx;
+	int saved_cy = E.cy;
+	int saved_coloff = E.coloff;
+	int saved_rowoff = E.rowoff;
+
+	char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)", editorFindCallback);
 
 	if (query) {
 		free(query);
+	} else {
+		E.cx = saved_cx;
+		E.cy = saved_cy;
+		E.coloff = saved_coloff;
+		E.rowoff = saved_rowoff;
 	}
 }
 
@@ -521,8 +550,9 @@ void editorDrawRows(struct abuf *ab) {
 void editorDrawStatusBar(struct abuf *ab) {
     abAppend(ab, "\x1b[7m", 4);
     char status[80], rstatus[80];
-    int len = snprintf(status, sizeof(status), "%.20s - %d lines %s",
-            E.filename ? E.filename : "[No Name]", E.numrows,
+    int len = snprintf(status, sizeof(status), "%s %.20s %s",
+			E.mode ? "[I]" : "[N]",
+            E.filename ? E.filename : "[No Name]",
             E.dirty ? "[+]" : "");
     int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",
             E.cy + 1, E.numrows);
